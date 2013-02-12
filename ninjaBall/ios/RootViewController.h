a625d8703bd7cf5490abcd4b6a5266ed91011d70
Dima <dmitriy.kirakosyan@gmail.com>
2013-02-12 14:43:55 +0400

    first commit

---
+++ ninjaBall/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ninjaBallAppController.h
+//  ninjaBall
+//
+//  Created by Dmitriy on 12/25/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
