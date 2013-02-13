4c525b1453727ea444446d1cb65fc935ba586e5e
Dima <dmitriy.kirakosyan@gmail.com>
2013-02-13 16:58:50 +0400

    small updates

--- ninjaBall/Classes/MainScene.cpp
+++ ninjaBall/Classes/MainScene.cpp
@@ -51,7 +51,7 @@ bool MainScene::init()
 
     // add a label shows "Hello World"
     // create and initialize a label
-    CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Thonburi", 34);
+    CCLabelTTF* pLabel = CCLabelTTF::create("Ninja Ball", "Thonburi", 34);
 
     // ask director the window size
     CCSize size = CCDirector::sharedDirector()->getWinSize();
@@ -59,17 +59,15 @@ bool MainScene::init()
     // position the label on the center of the screen
     pLabel->setPosition( ccp(size.width / 2, size.height - 20) );
 
+
+    CCSprite* background = CCSprite::create("MainMenuBG.png");
+    background->setPosition(ccp(size.width/2, size.height/2));
+    CCLog("size : %f, %f", size.width, size.height);
+    
+    this->addChild(background, 0);
+
     // add the label as a child to this layer
     this->addChild(pLabel, 1);
-
-    // add "HelloWorld" splash screen"
-    CCSprite* pSprite = CCSprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    pSprite->setPosition( ccp(size.width/2, size.height/2) );
-
-    // add the sprite as a child to this layer
-    this->addChild(pSprite, 0);
     
     return true;
 }
